86d7399e96b38c86884451818655282d2bfde4d5
Cindy Vo <yendai.vo@hotmail.com>
2018-07-10 19:00:07 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,12 @@ def countdown(integer)
   end
     "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(integer)
+  while integer > 0
+   "#{integer} SECOND(S)!"
+    integer -= 1
+    sleep(1)
+  end
+    "HAPPY NEW YEAR!"
+end
